32252322d07c2826714ac47025e86435466dfb35
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-21 00:51:39 +0300

    Сортировка

---
+++ Sort/Program.cs
@@ -0,0 +1,9 @@
+﻿// Сортировка
+double[] array = new double[8];
+for (int i = 0; i < array.Length; i++)
+  array[i] = Math.Round(new Random().NextDouble() * (50 - 10) + 10, 2);
+
+Array.Sort(array);
+
+Console.WriteLine($"[{string.Join(";", array)}]");
+Console.WriteLine(array[array.Length - 1] - array[0]);
