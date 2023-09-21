1c8ce53dfeb8109bd4d21e86015602ba9925ee35
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-21 22:53:21 +0300

    Проверка сторон треугольника

---
+++ TriangleSides/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и проверяет может ли существовать треугольник
+//  со сторонами такой длины(сумма двух сторон не должна быть больше третьей)
+void ReleaseArray(int[] array)
+{
+  if (array[0] < array[1] + array[2]
+      && array[1] < array[0] + array[2]
+      && array[2] < array[0] + array[1])
+    Console.Write("yes");
+  else
+    Console.Write("no");
+}
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+ReleaseArray(array);
