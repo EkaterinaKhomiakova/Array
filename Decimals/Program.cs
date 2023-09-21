b839bdbb526eec888a679e2395e3238527743713
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-21 23:16:41 +0300

    Код преобразования десятичного числа в двоичное

---
+++ Decimals/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string res = String.Empty;
+while (n > 0)
+{
+  res = Convert.ToString(n % 2) + res;
+  n /= 2;
+}
+Console.WriteLine(res);
+
