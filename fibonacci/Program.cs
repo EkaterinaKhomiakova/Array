bab9460a8a51f4de52f6ca634abc4523f34cf7d9
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-21 23:31:04 +0300

    Фибоначчи

---
+++ fibonacci/Program.cs
@@ -0,0 +1,17 @@
+﻿// Не используя рекурсию выведите первые N чисел фибоначчи
+// Первые два числа фибоначчи:0 и 1
+// Если N = 5 ->0 1 1 2 3
+// Если N = 3 ->0 1 1
+// Если N = 7 ->0 1 1 2 3 5 8
+// Console.WriteLine("Hello, World!");
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a0 = 0, a1 = 1, x;
+for (int i = 0; i < n; i++)
+{
+  Console.Write($"{a0}");
+  x = a0 + a1;
+  a0 = a1;
+  a1 = x;
+}
