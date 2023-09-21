0d01a31ef68a60932619143b4c0d180767b34a71
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-21 23:47:40 +0300

    Копия массива

---
+++ ArrayCopy/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет создавать копию 
+// заданного массива с помощью поэлементного копирования
+int[] a = { 1, 2, 3, 4 };
+int[] b = new int[a.Length];
+int[] c = new int[a.Length];
+for (int i = 0; i < a.Length; i++)
+  b[i] = a[i];
+b[0] = -5;
+Array.Copy(a, c, a.Length);
+c[0] = 78;
+Console.WriteLine($"[{string.Join(",", a)}]");
+Console.WriteLine($"[{string.Join(",", b)}]");
+Console.WriteLine($"[{string.Join(",", c)}]");
